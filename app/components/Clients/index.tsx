696bf4b8ed24639a4bedd98f6faec1988781a897
iqbalya25 <iqbalya25@gmail.com>
2024-05-16 15:15:06 +0700

    add

--- app/components/Clients/index.tsx
+++ app/components/Clients/index.tsx
@@ -14,7 +14,7 @@ const Clients: React.FC = () => {
             src={client.logo}
             alt={client.name}
             className="inline-block w-32 h-auto animate-marquee"
-            style={{ animationDuration: "1s" }}
+            style={{ animationDuration: "5s" }}
           />
         ))}
       </div>
